f954530800b7c36960311e7ebe466639a0da9369
Soumadeep Singha <soumadeep1@gmail.com>
2021-05-14 13:52:28 +0530

    First Commit

---
+++ palindrome.py
@@ -0,0 +1,19 @@
+"""
+Checking a string is palindrome or not
+"""
+
+s=input()
+j=len(s)-1
+i=0
+flag=0
+while(i<j):
+    if(s[i]!=s[j]):
+        flag=1
+        break
+    else:
+        i+=1
+        j-=1
+if(flag==0):
+    print("palindrome")
+else:
+    print("not palindrome")
